c316206717640a6aaa9e24e4489d743e28605378
praveen <praveenpatel050@gmail.com>
2024-05-24 18:20:40 +0530

    all file

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../../assets/images/logo.png";
-
+import "./header.css";
 import { Container, Row, Button } from "reactstrap";
 import { NavLink, Link } from "react-router-dom";
 
@@ -11,7 +11,7 @@ const nav_links = [
   },
 
   {
-    path: "#",
+    path: "/about",
     display: "About",
   },
 
@@ -36,7 +36,7 @@ const Header = () => {
               <ul className="menu d-flex align-items-center gap-5">
                 {nav_links.map((item, index) => (
                   <li className="nav__item" key={index}>
-                    <NavLink to={item.path}>{item.display}</NavLink>
+                    <NavLink to={item.path} className={navClass => navClass.isActive ? 'active__link' : ''}>{item.display}</NavLink>
                   </li>
                 ))}
               </ul>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React from "react";
+import "../styles/home.css";
+import { Container, Row, Col } from "reactstrap";
+import heroImg from "../assets/images/hero-img01.jpg";
+import heroImg2 from "../assets/images/hero-img02.jpg";
+import heroVideo from "../assets/images/hero-video.mp4";
+import worldImg from "../assets/images/world.png";
+import Subtitle from "./../shared/Subtitle";
+
+import SearchBar from "../shared/SearchBar";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <>
+      {/* ====hero Section start===== */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <div className="hero__content">
+                <div className="hero__subtitle d-flex align-items-center ">
+                  <Subtitle subtitle={"Know Before You Go"} />
+                  <img src={worldImg} alt="" />
+                </div>
+                <h1>
+                  Lab Opens the door to creating
+                  <span className="highlight"> Memories</span>
+                </h1>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Quasi modi rerum, quod totam error dolore qui hic fugiat
+                  repellat exercitationem, odio animi similique cupiditate
+                  voluptatibus aliquid rem impedit quisquam beatae.
+                </p>
+              </div>
+            </Col>
 
-export default Home
+            <Col lg="2">
+              <div className="hero__img-box">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-4">
+                <video src={heroVideo} alt="" controls />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-5">
+                <img src={heroImg2} alt="" />
+              </div>
+            </Col>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+      {/* ====hero section end===== */}
+    </>
+  );
+};
+
+export default Home;
